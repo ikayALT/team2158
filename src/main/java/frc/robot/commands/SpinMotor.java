e01a3533ceb7f3c7ee166d84b3c767647e57eff8
ikayALT <kyekellercnatx@gmail.com>
2024-09-26 17:33:21 -0500

    2158

---
+++ src/main/java/frc/robot/commands/SpinMotor.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.SpinMotorSub;
+
+public class SpinMotor extends Command {
+  SpinMotorSub spinMotor_Sub;
+
+  double intakeSpeed = Constants.intakeSpeed;
+  double shootSpeed = Constants.shootSpeed;
+  double stopSpeed = Constants.stopSpeed;
+
+  public static boolean shootReady = false;
+  boolean botFirstAction = false; 
+
+  /** Creates a new SpinMotor. */
+  public SpinMotor(SpinMotorSub spinMotor_Sub) {
+    this.spinMotor_Sub = spinMotor_Sub;
+    addRequirements(this.spinMotor_Sub);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    
+    if (shootReady == false) {
+      if (botFirstAction == false)
+      {
+        System.out.println("intake ready!");
+        System.out.println("!! This is the bots first action since start up !!");
+        botFirstAction = true;
+        shootReady = false;
+      }
+      else {
+        System.out.println("shoot ready!");
+        shootReady = true;
+      }
+    }
+    else{
+      System.out.println("intake ready!");
+      shootReady = false;
+    }
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (shootReady == true) {
+      spinMotor_Sub.setMotorSpeed(shootSpeed);
+    }
+    else{
+      spinMotor_Sub.setMotorSpeed(intakeSpeed);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    spinMotor_Sub.setMotorSpeed(stopSpeed);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/SpinMotorSub.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import org.opencv.core.Mat;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class SpinMotorSub extends SubsystemBase {
+  CANSparkMax m_motor11 = Constants.m_motor11;
+  //CANSparkMax m_motor14 = Constants.m_motor14;
+
+  /** Creates a new SpinMotorSub. */
+  int randInt;
+  public SpinMotorSub() {}
+
+  @Override
+  public void periodic() {
+    randInt = ((int) (Math.random() * 10) + 1);
+    // this replaces my gambling addication
+  }
+
+  public void setMotorSpeed(double speed) {
+    m_motor11.set(speed);
+    //m_motor14.set(speed);
+  }
+
+  public boolean startMotor() {
+    return true;
+  }
+}
