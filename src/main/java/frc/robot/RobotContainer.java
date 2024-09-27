10765f072d82ce3eb30aa6de5f905284fad02871
ikayALT <kyekellercnatx@gmail.com>
2024-09-27 16:39:03 -0500

    fixed errors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,8 @@ package frc.robot;
 //import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.IncreaseSpeed;
-//import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.SpinMotor;
-import frc.robot.commands.IncreaseSpeed;
+//import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.SpinMotorSub;
 
